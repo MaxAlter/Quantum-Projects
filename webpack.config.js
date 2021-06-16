8dd65ecdac5cc72506475f3453ba0847202da04a
MaxAlter <alter0075926@gmail.com>
2021-06-17 01:40:27 +0300

    fixed bags file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,74 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const filename = (ext) =>
+  isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
 module.exports = {
-  entry: "./src/index.js",
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: "./js/main.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: `./js/${filename("js")}`,
+    path: path.resolve(__dirname, "app"),
+    publicPath: "",
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, "app"),
+    historyApiFallback: true,
+    hot: false,
+    compress: true,
+    open: true,
+    port: 9080,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+      filename: "index.html",
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: `./css/${filename("css")}`,
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
+        test: /\.html$/i,
+        loader: "html-loader",
       },
       {
         test: /\.css$/i,
         use: [
-          // "style-loader",
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            // options: {
+            //   hmr: isDev,
+            // },
+          },
           "css-loader",
-          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.(?:|gif|png|jpg|jpeg|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: `./img/${filename("[ext]")}`,
+            },
+          },
         ],
       },
     ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "bundle.css",
-    }),
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    publicPath: "",
-    historyApiFallback: true,
-    noInfo: true,
-    quiet: true,
-    clientLogLevel: "warning",
-    stats: "errors-only",
-    compress: true,
-    open: true,
-    port: 9090,
-  },
-  // devtool: "eval-cheap-source-map	",
 };
+// name: `./img/${filename("[ext]")}`,  //  image/[name].[ext] //  name: "./img/[name].[ext]",
